4fb266c9fe583e171f2f01f7d27be0ce04b322a0
chux0519 <chuxdesign@hotmail.com>
2019-10-06 14:29:25 +0800

    feat: make rgba field of color public

--- src/lib.rs
+++ src/lib.rs
@@ -105,17 +105,17 @@ impl TGAColor {
 
 #[derive(Copy, Clone, Debug)]
 pub struct TGAColorRGB {
-    r: u8,
-    g: u8,
-    b: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct TGAColorRGBA {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 #[derive(Clone)]
